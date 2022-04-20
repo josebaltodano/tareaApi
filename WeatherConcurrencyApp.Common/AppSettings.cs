635a55eda2da35702d6e347c94a3898dec7d1a70
roberto-sanchez06 <sanrobb18@gmail.com>
2022-04-20 08:17:15 -0600

    Modificando proyecto

--- WeatherConcurrencyApp.Common/AppSettings.cs
+++ WeatherConcurrencyApp.Common/AppSettings.cs
@@ -11,6 +11,6 @@ namespace WeatherConcurrencyApp.Common
     {
         public static string ApiUrl { get => ConfigurationManager.AppSettings.Get("ApiUrl"); }
         public static string Token { get => ConfigurationManager.AppSettings.Get("Token"); }
-        public static string units { get => ConfigurationManager.AppSettings.Get("Units"); }
+        public static string Units { get => ConfigurationManager.AppSettings.Get("Units"); }
     }
 }
